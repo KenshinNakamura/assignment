cd33664655ba622d7d1d5bf2db069f2e6b600421
KenshinNakamura <kenshin_nakamura@cider.systems>
2023-10-30 21:31:58 +0900

    進捗データ

--- 55.py
+++ 55.py
@@ -1,9 +1,39 @@
 #No.55 夢想花again
 #「とんで」を9回「まわって」を3回繰り返した後「まわる」と表示して改行する、を3回繰り返すプログラムを作成せよ。
 #「とんで」「まわって」と3行文の繰り返しは必ず繰り返し構文を使うこと。
-
+num0 = 0
+num1 = 0
+num2 = 0
+num3 = 0
 var_1 = "とんで"
 var_2 = "まわって"
 var_3 = "まわる"
-for var_10 in  range(3):
-    print(f"{var_1*9} {var_2*3} {var_3}")
+
+    #for var_5 in range(0):
+while num1 < 8:
+    var_1 += "とんで"
+    num1 += 1
+
+while num2 < 3:
+    var_2 += "まわって"
+    num2  += 1
+
+result =var_1+var_2+var_3
+
+for var_4 in range(3):
+    print(result)
+
+
+#print(f"{var_1}{var_2}{var_3}")
+
+        #print(var_1,var_2)
+
+#var_1 = "とんで"
+#var_2 = "まわって"
+#var_3 = "まわる"
+
+#for var_4 in  range(3):
+    #print(f"{var_1*9} {var_2*3} {var_3}")
+
+#i,j,k など
+#date57.csvという名前のデータファイルを作成する
